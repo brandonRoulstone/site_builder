4f7c051f8d8dcd5ed3afc5799fda784eec10a70a
FrostXk9 <brandonroulstone465@gmail.com>
2024-05-03 06:39:56 +0200

    igt

--- src/Components/DashBoard.jsx
+++ src/Components/DashBoard.jsx
@@ -2,17 +2,20 @@ import React from 'react'
 import { Nav } from 'react-bootstrap'
 import { Link } from 'react-router-dom'
 import { useState, useEffect } from 'react'
+// import addTaskIcon from './icons/addTaskIcon.jsx'
 import axios from 'axios'
+import { PropTypes } from 'prop-types';
 
 const Dashboard = () => {
   const [isClosed, setClosed] = useState(false);
+  const [isChecked, setIsChecked] = useState(true);
   const [taskData, setTaskData] = useState([]);
   // const [isCompleted, setCompleted] = useState(false)
 
   const fetchdata = async () => {
     try {
-      const data = await axios.get('http://localhost:3360/tasks');
-      setTaskData(data.data)
+      const {data} = await axios.get('http://localhost:3360/tasks');
+      setTaskData(data)
     } catch (error) {
       console.log(error)
     }
@@ -23,7 +26,7 @@ const Dashboard = () => {
 
   const toggleSideBar = () => {
     let elem = document.getElementById("sideBar");
-    elem.style='left: -50%';
+    elem.style='left: -100%';
     setClosed(true);
     console.log(isClosed);
   }
@@ -36,6 +39,38 @@ const Dashboard = () => {
       console.log(isClosed);
     }
   }
+
+  const [addNexTask, setAdd] = useState({
+    taskname: '',
+    taskdeadline: '',
+    completed: false,
+    created: ''
+  });
+  
+  const addTask = async (e) => {
+    e.preventDefault();
+    try {
+      const { data } = await axios.post('http://localhost:3360/tasks', {...addNexTask, [e.target.name]: e.target.value});
+      console.log('Task added:', data);
+      // setAdd(
+      //   { ...addNexTask, 
+      //   [e.target.name]: e.target.value,
+      // }
+      // );
+    } catch (error) {
+      console.error('Error adding task:', error);
+    }
+  }
+
+  const handleChange = (e) => {
+    setAdd({...addNexTask, [e.target.name]: e.target.value,});
+    console.log(e.target.value);
+  };
+
+
+  const handleCheckboxChange = (event) => {
+    setIsChecked(event.target.checked);
+  };
 
   return (
     <>
@@ -109,13 +144,25 @@ const Dashboard = () => {
             <Nav.Link to="/" as={Link} id='bottomNav'>Home</Nav.Link>
           </Navbar> */}
 
+          <div className="toolbar">
+            <button className='btn' onClick={addTask}>
+              <svg xmlns="http://www.w3.org/2000/svg" width="25" height="25" fill="currentColor" className="bi bi-plus-circle-fill" viewBox="0 0 16 16">
+               <path d="M16 8A8 8 0 1 1 0 8a8 8 0 0 1 16 0M8.5 4.5a.5.5 0 0 0-1 0v3h-3a.5.5 0 0 0 0 1h3v3a.5.5 0 0 0 1 0v-3h3a.5.5 0 0 0 0-1h-3z"/>
+              </svg>
+            </button>
+          </div>
+
           { isClosed === false ? <button id='close' onClick={toggleSideBar}>
-            x
+          <svg xmlns="http://www.w3.org/2000/svg" width="16" height="16" fill="currentColor" className="bi bi-arrow-left-circle" viewBox="0 0 16 16">
+              <path fillRule="evenodd" d="M1 8a7 7 0 1 0 14 0A7 7 0 0 0 1 8m15 0A8 8 0 1 1 0 8a8 8 0 0 1 16 0m-4.5-.5a.5.5 0 0 1 0 1H5.707l2.147 2.146a.5.5 0 0 1-.708.708l-3-3a.5.5 0 0 1 0-.708l3-3a.5.5 0 1 1 .708.708L5.707 7.5z"/>
+            </svg>
           </button> : <button id='close' onClick={openSideBar}>
-            {">"}
+            <svg xmlns="http://www.w3.org/2000/svg" width="16" height="16" fill="currentColor" className="bi bi-arrow-right-circle" viewBox="0 0 16 16">
+              <path fillRule="evenodd" d="M1 8a7 7 0 1 0 14 0A7 7 0 0 0 1 8m15 0A8 8 0 1 1 0 8a8 8 0 0 1 16 0M4.5 7.5a.5.5 0 0 0 0 1h5.793l-2.147 2.146a.5.5 0 0 0 .708.708l3-3a.5.5 0 0 0 0-.708l-3-3a.5.5 0 1 0-.708.708L10.293 7.5z"/>
+            </svg>
           </button>}
 
-          <div className='container-fluid'>
+          <div className='container'>
             {taskData.map((value) => {
               return <div key={value.taskId} className='d-flex justify-content-center container-fluid mt-5'>
                 <div id='taskCard' className='bg-white w-50 mt-3'>
@@ -123,8 +170,8 @@ const Dashboard = () => {
                     <p>{value.taskdeadline}</p>
                     {
                       value.completed >= 1 
-                      ? (<input type="checkbox" checked id="checkbox" value={value.completed} />) 
-                      : ( <input type="checkbox" id="checkbox" value={value.completed} />)
+                      ? (<input type="checkbox" checked={isChecked} onChange={handleCheckboxChange} id="checkbox" value={value.completed} />) 
+                      : ( <input type="checkbox" onChange={handleCheckboxChange} id="checkbox" value={value.completed} />)
                     }
                   </div>
                   <h5 className='fw-bold'>{value.taskname}</h5>
@@ -144,11 +191,43 @@ const Dashboard = () => {
                   </ul>
                 </div>
               </div>
-              {/* <div className='card py-5 w-100' id='cardXv'>
-                assigned
-              </div> */}
               <div className='card py-5 w-100' id='cardXv'>
-                messages
+                <form className='adjust_alignment'>
+                  <input
+                    type="text"
+                    name="name"
+                    placeholder="Task Name"
+                    defaultValue={addNexTask.taskname}
+                    onChange={handleChange}
+                    className='mb-3 styledInput'
+                  />
+                  <input
+                    name="deadline"
+                    placeholder="Task deadline"
+                    defaultValue={addNexTask.taskdeadline}
+                    onChange={handleChange}
+                    className='mb-3 styledInput'
+                  />
+                  <input
+                    name="completed"
+                    placeholder="Task completion"
+                    defaultValue={addNexTask.completed}
+                    onChange={handleChange}
+                    className='mb-3 styledInput'
+                  />
+                  <input
+                    name="created"
+                    placeholder="Created"
+                    defaultValue={addNexTask.created}
+                    onChange={handleChange}
+                    className='mb-3 styledInput'
+                  />
+                  <button type="button" className='primary_styled_btn' onClick={addTask}>
+                    Add Task  <svg xmlns="http://www.w3.org/2000/svg" width="16" height="16" fill="#9454ee" className="bi bi-plus-circle-fill" viewBox="0 0 16 16">
+                      <path d="M16 8A8 8 0 1 1 0 8a8 8 0 0 1 16 0M8.5 4.5a.5.5 0 0 0-1 0v3h-3a.5.5 0 0 0 0 1h3v3a.5.5 0 0 0 1 0v-3h3a.5.5 0 0 0 0-1h-3z"/>
+                    </svg>
+                  </button>
+                </form>
               </div>
             </div>
         </div>
